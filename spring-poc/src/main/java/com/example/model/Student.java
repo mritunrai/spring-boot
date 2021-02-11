cf5af10fd6f84bd4d4f5bcef754737796e85a6ce
Mritunjai Rai <mkrai@Mritunjais-MacBook-Pro.local>
2021-02-11 16:36:51 +0530

    added spring boot rest api

---
+++ spring-poc/src/main/java/com/example/model/Student.java
@@ -0,0 +1,32 @@
+package com.example.model;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getMobile() {
+		return mobile;
+	}
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+	private String email;
+	private int mobile;
+
+}
