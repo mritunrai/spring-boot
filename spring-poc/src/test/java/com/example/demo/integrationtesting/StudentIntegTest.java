20d7135c57db61b689cc645f34c97812543ae7b9
Mritunjai Rai <mkrai@Mritunjais-MacBook-Pro.local>
2021-02-24 21:31:45 +0530

    added integration test

--- spring-poc/src/test/java/com/example/demo/integrationtesting/StudentIntegTest.java
+++ spring-poc/src/test/java/com/example/demo/integrationtesting/StudentIntegTest.java
@@ -1,39 +1,62 @@
 package com.example.demo.integrationtesting;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.matching;
+import static com.github.tomakehurst.wiremock.client.WireMock.notMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 
-import com.example.services.StudentService;
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
 
 public class StudentIntegTest {
 	
-	private StudentService studentService;
-	
-	private WireMockServer wireMockServer;
-	
-	 @BeforeEach
-	    void setup() {
-	        wireMockServer = new WireMockServer();
-	        WireMock.configureFor("localhost", 8080);
-	        wireMockServer.start();
-	    }
-	
-	
-	@Test
-	public void exactUrlOnly() {
-		  WireMock.configureFor("localhost", 8080);
-		  
-		stubFor(get(urlEqualTo("/students"))
-				.willReturn(aResponse()
-						.withHeader("Content-Type", "text/plain")
-						.withBody(fileToJSON("student_get_response.json"))));
-	}
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(8089);
 
-	@AfterEach
-    void tearDown() {
-        wireMockServer.stop();
+    
+    @BeforeClass
+    public  void setUpClass() {
+    	wireMockRule.start();
+    }
+
+    @AfterClass
+    public  void tearDownClass() {
+    	wireMockRule.stop();
+    }
+    
+    @Test
+    @Tag("integ")
+    public void exampleTest() {
+        stubFor(get(urlEqualTo("/my/resource"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "text/xml")
+                    .withBody("<response>Some content</response>")));
+
+//        Result result = myHttpServiceCallingObject.doSomething();
+//
+//        assertTrue(result.wasSuccessful());
+
+        verify(postRequestedFor(urlMatching("/my/resource/[a-z0-9]+"))
+                .withRequestBody(matching(".*<message>1234</message>.*"))
+                .withHeader("Content-Type", notMatching("application/json")));
     }
 }
